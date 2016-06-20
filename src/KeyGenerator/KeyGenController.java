a1495e664d48cef9559f4f8b1c6da6c24b0974a3
Bart van Keersop <bart.van.keersop@gmail.com>
2016-06-20 13:49:18 +0200

    Generating Public and Private Key now works

--- src/KeyGenerator/KeyGenController.java
+++ src/KeyGenerator/KeyGenController.java
@@ -1,20 +1,50 @@
 package KeyGenerator;
 
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.security.*;
 
 /**
  * Created by Bart van Keersop on 6/20/2016.
  */
 public class KeyGenController {
 
-    KeyPairGenerator keyGen;
-    public static final String ALGORITHM = "RSA";
+    private final KeyPair key;
+    private KeyPairGenerator keyGen;
+    private static final String ALGORITHM = "RSA";
+    private static final String PUBLIC_KEY_FILE = "C:\\Users\\Bart van Keersop\\Documents\\GitHub\\SE42Encryption\\files\\public.key";
+    private static final String PRIVATE_KEY_FILE = "C:\\Users\\Bart van Keersop\\Documents\\GitHub\\SE42Encryption\\files\\private.key";
 
     public KeyGenController() throws NoSuchProviderException, NoSuchAlgorithmException {
         keyGen = KeyPairGenerator.getInstance(ALGORITHM);
+        keyGen.initialize(1024);
+        key = keyGen.generateKeyPair();
+
+        try {
+            writePublicKey();
+            writePrivateKey();
+        } catch (IOException e) {
+            System.out.println("------Error trying to write the keyfiles------ \n");
+            e.printStackTrace();
+        }
     }
 
-    
+    public void writePublicKey() throws IOException {
+        File publicKeyFile = new File(PUBLIC_KEY_FILE);
+        publicKeyFile.createNewFile();
+        ObjectOutputStream publicKeyOS = new ObjectOutputStream(new FileOutputStream(publicKeyFile));
+        publicKeyOS.writeObject(key.getPublic());
+        publicKeyOS.close();
+
+    }
+
+    public void writePrivateKey() throws IOException {
+        File privateKeyFile = new File(PRIVATE_KEY_FILE);
+        privateKeyFile.createNewFile();
+        ObjectOutputStream privateKeyOS = new ObjectOutputStream(new FileOutputStream(privateKeyFile));
+        privateKeyOS.writeObject(key.getPrivate());
+        privateKeyOS.close();
+    }
 }
